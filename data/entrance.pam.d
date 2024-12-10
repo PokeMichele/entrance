4d57017ddb98fa09f2eb0b5b0bbf0c0ae4a569e8
William L. Thomson Jr. <w@wltjr.com>
2024-12-10 13:05:23 -0500

    data: Update pam_kwallet library version to 6, probably should drop version

--- data/entrance.pam.d
+++ data/entrance.pam.d
@@ -2,10 +2,10 @@
 
 auth		include		system-login
 -auth		optional	pam_gnome_keyring.so
--auth		optional	pam_kwallet5.so
+-auth		optional	pam_kwallet6.so
 account		include		system-login
 password	include		system-login
 session		include		system-login
 -session	optional	pam_gnome_keyring.so auto_start
--session	optional	pam_kwallet5.so auto_start
+-session	optional	pam_kwallet6.so auto_start
 
